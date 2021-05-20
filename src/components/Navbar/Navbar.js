a209bf313bb19ac37378ee9c8a00134abc7fd02f
Akash Kumar <akaysharma894@gmail.com>
2021-05-20 16:40:23 +0530

    Added Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactLogo from "../assets/images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  Navbarcontainer,
+  Navlogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn1,
+  NavBtn2,
+  NavBtnLink1,
+  NavBtnLink2,
+} from "../Navbar/NavbarElement";
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <Navbarcontainer>
+        <Navlogo to="">
+          <img src={ReactLogo} alt="react-logo" />
+        </Navlogo>
+        <MobileIcon>
+          <FaBars />
+        </MobileIcon>
+        <NavMenu>
+          <NavItem>
+            <NavLinks to="Features">Features</NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="Pricing">Pricing</NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="Resources">Resources</NavLinks>
+          </NavItem>
+        </NavMenu>
+        <NavBtn1>
+          <NavBtnLink1 to="Login">Login</NavBtnLink1>
+        </NavBtn1>
+        <NavBtn2>
+          <NavBtnLink2 to="Signup">Sign Up</NavBtnLink2>
+        </NavBtn2>
+      </Navbarcontainer>
+    </Nav>
+  );
+};
+
+export default Navbar;
