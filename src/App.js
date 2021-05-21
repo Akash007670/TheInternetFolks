546a05caaafe3a8d97bc305062bd400ec7f02f99
Akash Kumar <akaysharma894@gmail.com>
2021-05-21 17:36:31 +0530

    updated path

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/Home";
+import Start from "./components/GetStarted/Start";
+import Searchbar from "./components/MainContent/Searchbar";
+
 
 const App = () => {
   return (
     <div className="App">
       <Router>
         <Home />
+        <Start />
       </Router>
+      <Searchbar />
     </div>
   );
 };
