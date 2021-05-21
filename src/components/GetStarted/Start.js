4e6bd6d4cffbe715e2c739be435f861a298fb182
Akash Kumar <akaysharma894@gmail.com>
2021-05-21 17:34:33 +0530

    Added Getting started

---
+++ src/components/GetStarted/Start.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Workingperson from "../assets/images/illustration-working.svg";
+
+import {
+  Description,
+  Heading,
+  Para,
+  Person,
+  BtnWrapper,
+  Button,
+  ContentWrapper,
+} from "../GetStarted/GetStartedElement";
+
+const Start = () => {
+  return (
+    <>
+      <Description>
+        <ContentWrapper>
+          <Heading>More than just shorter links</Heading>
+          <Para>
+            Build your brand's recognition and get detailed insights on how your
+            links are performing.
+          </Para>
+          <BtnWrapper>
+            <Button to="Signup">Get Started</Button>
+          </BtnWrapper>
+        </ContentWrapper>
+        <Person>
+          <img height = {400} width = {600} src={Workingperson} alt="working-person" />
+        </Person>
+      </Description>
+    </>
+  );
+};
+
+export default Start;
