8cf46aeb3bd7a56fad6454c415f02f87a1085f12
Akash Kumar <akaysharma894@gmail.com>
2021-05-22 19:08:35 +0530

    Added searchbarElement

--- src/components/MainContent/SearchbarElement.js
+++ src/components/MainContent/SearchbarElement.js
@@ -1,87 +1,153 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Background = styled.div`
-display : flex;
-margin-top : 30rem;
-margin-left : 9rem;
-margin-right : 12rem;
-background-color : #3b3054;
-position : sticky;
-border-radius : 20px;
-margin-bottom : 2rem;
+  display: flex;
+  margin-top: 30rem;
+  margin-left: 9rem;
+  margin-right: 12rem;
+  background-color: #3b3054;
+  position: sticky;
+  border-radius: 20px;
+  margin-bottom: 2rem;
+  z-index : 999;
 
-@media screen and (max-width : 760px) {
-display : flex;
-flex: 100;
-margin-top: 10px;
-padding: 10px;
-width : 85%;
-position : absolute;
-left : -130px;
-margin-bottom : 2rem;
-}
+  @media screen and (max-width: 760px) {
+    display: flex;
+    flex: 100;
+    margin-top: 10px;
+    padding: 10px;
+    width: 90%;
+    position: absolute;
+    left: -125px;
+    margin-bottom: 2rem;
+    z-index : 0;
+  }
 `;
 
 export const SearchBar = styled.input`
-display : flex;
-position : absolute;
-top : 2rem;
-left : 2rem;
-align-items :center;
-justify-content : center;
-border : none;
-outline : none;
-font-size : 18px;
-width : 668px;
-height : 40px;
-margin : 10px;
-padding : 10px;
-border-radius : 5px;
+  display: flex;
+  position: absolute;
+  top: 2rem;
+  left: 2rem;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  outline: none;
+  font-size: 18px;
+  width: 668px;
+  height: 40px;
+  margin: 10px;
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
 
-@media screen and (max-width : 760px) {
-    display : flex;
-    width : 240px;
-    left : 2px;
-    top : 1rem;
-    flex  :100;
-    order : 1;
-}
+  @media screen and (max-width: 760px) {
+    display: flex;
+    width: 90%;
+    left: 2px;
+    top: 1rem;
+    flex: 100;
+    order: 1;
+  }
 `;
 
-
 export const ButtonWrapper = styled.div`
-display : flex;
-align-items : center;
-position  :absolute;
-top : 2.3rem;
-right : 2rem;
-@media screen and (max-width : 760px) {
-    display : flex;
-    width : 300px;
-    flex : 100;
-    order : 2;
-    top : 100px;
-    left : 11px;
-    padding-bottom : 1.5rem;
-}
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: 2.3rem;
+  right: 2rem;
+  @media screen and (max-width: 760px) {
+    display: flex;
+    width: 100%;
+    flex: 100;
+    order: 2;
+    top: 100px;
+    left: 11px;
+    padding-bottom: 1.5rem;
+  }
 `;
 
 export const Button = styled.button`
-border-radius : 8px;
-color : white;
-background-color : #2acfcf;
-white-space : nowrap;
-padding : 10px 30px;
-font-size : 18px;
-cursor : pointer;
-outline : none;
-border : none;
-text-decoration : none;
-@media screen and (max-width : 760px) {
-    display : flex;
-    height : 40px;
-    width : 240px;
-    align-items : center;
-    justify-content : center;
-}
+  border-radius: 8px;
+  color: white;
+  background-color: #2acfcf;
+  white-space: nowrap;
+  padding: 10px 30px;
+  font-size: 18px;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  text-decoration: none;
+  @media screen and (max-width: 760px) {
+    display: flex;
+    height: 40px;
+    width: 90%;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const LinkList = styled.div`
+  display: flex;
+  background-color: #EFF1F7;
+  border-radius: 8px;
+  margin-left: 9rem;
+  margin-right: 12rem;
+  margin-bottom: 2rem;
+  height: 60px;
+  align-items: center;
+`;
+
+export const CopyBtn = styled.button`
+  display: flex;
+  position: absolute;
+  border-radius: 8px;
+  color: white;
+  background-color: #2acfcf;
+  white-space: nowrap;
+  padding: 10px 30px;
+  font-size: 18px;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  right: 14.1rem;
+  text-decoration: none;
+//   display: none;
+  // @media screen and (max-width : 760px) {
+  //     display : flex;
+  //     height : 40px;
+  //     width : 240px;
+  //     align-items : center;
+  //     justify-content : center;
+  // }
+`;
+
+export const CopiedBtn = styled.button`
+  display: flex;
+  position: absolute;
+  border-radius: 8px;
+  color: white;
+  background-color: #2acfcf;
+  white-space: nowrap;
+  padding: 10px 30px;
+  font-size: 18px;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  text-decoration: none;
+  right: 14.1rem;
+  // display : none;
+
+
+  // @media screen and (max-width : 760px) {
+  //     display : flex;
+  //     height : 40px;
+  //     width : 240px;
+  //     align-items : center;
+  //     justify-content : center;
+  // }
+`;
+export const Paragraph = styled.p`
+margin-left: 2em;
 `;
